092c935d7b2433dda9081749eee48054b3f47aac
Blake webm <74615355+Blake-webm@users.noreply.github.com>
2021-02-11 20:04:29 +0300

    Trosh

--- Piece/Pawn.py
+++ Piece/Pawn.py
@@ -1,26 +1,47 @@
 from Piece.Piece import Piece
 from Ceil import Ceil
 
-class Pawn(Piece):
-
     def can_move(self, position, board):
-        if position.y - self.y == 1 and position.x - self.x == 0 or position.y - self.y == 2 and position.x - self.x == 0:
-            opponent = board.get_figure(Ceil(position.x, position.y))
-            if opponent:
-                if opponent.y - position.y == 1 or opponent.y - position.y == -1:
-                    return False
-                if self.color == opponent.color:
-                    return False
-                if self.color != opponent.color:
-                    return False
-            return True
-        if position.y - self.y == -1 and position.x - self.x == 0 or  position.y - self.y == -2 and position.x - self.x == 0:
-            opponent = board.get_figure(Ceil(position.x, position.y))
-            if opponent:
-                if position.y :
-                    return False
-                if self.color == opponent.color:
-                    return False
-                if self.color != opponent.color:
-                    return False
-            return True
+        if self.y == 1:
+            if position.y - self.y == 1 or position.y - self.y == 2:
+                opponent = board.get_figure(Ceil(position.x, position.y))
+                if opponent:
+                    if opponent.x == position.x and opponent.y == position.y:
+                        return True
+                    if self.color == opponent.color:
+                        return False
+                    if self.color != opponent.color:
+                        return False
+                    if position.x - self.x == 0:
+                        return False
+                return True
+        else:
+            if position.y - self.y == 1 and position.x - self.x == 0:
+                opponent = board.get_figure(Ceil(position.x, position.y))
+                if opponent:
+                    if self.color == opponent.color:
+                        return False
+                    if self.color != opponent.color:
+                        return False
+                return True
+        if self.y == 6:
+            if position.y - self.y == -1 and position.x - self.x == 0 or  position.y - self.y == -2 and position.x - self.x == 0:
+                opponent = board.get_figure(Ceil(position.x, position.y))
+                if opponent:
+                    if opponent.y != 5:
+                        return False
+                    if self.color == opponent.color:
+                        return False
+                    if self.color != opponent.color:
+                        return False
+                return True
+        else:
+            if position.y - self.y == -1 and position.x - self.x == 0:
+                opponent = board.get_figure(Ceil(position.x, position.y))
+                if opponent:
+                    if self.color == opponent.color:
+                        return False
+                    if self.color != opponent.color:
+                        return False
+                return True
+
