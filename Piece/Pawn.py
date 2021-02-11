7a4646a875b704c3975bc2344d4dba5c19ceec1d
Blake webm <74615355+Blake-webm@users.noreply.github.com>
2021-02-11 20:10:23 +0300

    Trosh

--- Piece/Pawn.py
+++ Piece/Pawn.py
@@ -3,16 +3,12 @@ from Ceil import Ceil
 
     def can_move(self, position, board):
         if self.y == 1:
-            if position.y - self.y == 1 or position.y - self.y == 2:
+            if position.y - self.y == 1 and position.x - self.x == 0 or position.y - self.y == 2 and position.x - self.x == 0:
                 opponent = board.get_figure(Ceil(position.x, position.y))
                 if opponent:
-                    if opponent.x == position.x and opponent.y == position.y:
-                        return True
                     if self.color == opponent.color:
                         return False
                     if self.color != opponent.color:
-                        return False
-                    if position.x - self.x == 0:
                         return False
                 return True
         else:
@@ -28,8 +24,6 @@ from Ceil import Ceil
             if position.y - self.y == -1 and position.x - self.x == 0 or  position.y - self.y == -2 and position.x - self.x == 0:
                 opponent = board.get_figure(Ceil(position.x, position.y))
                 if opponent:
-                    if opponent.y != 5:
-                        return False
                     if self.color == opponent.color:
                         return False
                     if self.color != opponent.color:
